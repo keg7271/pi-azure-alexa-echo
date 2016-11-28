b0db21bcde2ca3427d0106286373ba1545206925
Keith Geringer <keg7271@gmail.com>
2016-11-27 18:10:41 -0700

    next update

--- pi-azure-alexa/PiAzureAlexa.Skills.IoTCore/StartupTask.cs
+++ pi-azure-alexa/PiAzureAlexa.Skills.IoTCore/StartupTask.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Net.Http;
 using Windows.ApplicationModel.Background;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
 
 // The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
 
@@ -11,7 +14,11 @@ namespace PiAzureAlexa.Skills.IoTCore
 {
     public sealed class StartupTask : IBackgroundTask
     {
-        public void Run(IBackgroundTaskInstance taskInstance)
+        static DeviceClient deviceClient;
+        static string iotHubUri = "RCDogz.azure-devices.net";
+        static string deviceKey = "dHUKUejhahpaHJfvHi+5tjsS1GrJjmYRqXuevmPuTHY=";
+
+        public async void Run(IBackgroundTaskInstance taskInstance)
         {
             // 
             // TODO: Insert code to perform background work
@@ -20,6 +27,40 @@ namespace PiAzureAlexa.Skills.IoTCore
             // from closing prematurely by using BackgroundTaskDeferral as
             // described in http://aka.ms/backgroundtaskdeferral
             //
+            var deferral = taskInstance.GetDeferral();
+
+            deviceClient = DeviceClient.Create(iotHubUri, new DeviceAuthenticationWithRegistrySymmetricKey("RCDogzPi1", deviceKey));
+
+            await SendDeviceToCloudMessagesAsync();
+
+            deferral.Complete();
         }
+
+        private static async Task SendDeviceToCloudMessagesAsync()
+        {
+            int diceRole = 7; // m/s
+            Random rand = new Random();
+
+            while (true)
+            {
+                diceRole = rand.Next(2, 12);
+
+                var telemetryDataPoint = new
+                {
+                    deviceId = "RCDogzPi1",
+                    diceRole = diceRole,
+                    dateTime = DateTime.UtcNow
+                };
+
+                var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+                var message = new Message(Encoding.ASCII.GetBytes(messageString));
+
+                await deviceClient.SendEventAsync(message);
+                //Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
+
+                Task.Delay(60000).Wait();
+            }
+        }
+
     }
 }
