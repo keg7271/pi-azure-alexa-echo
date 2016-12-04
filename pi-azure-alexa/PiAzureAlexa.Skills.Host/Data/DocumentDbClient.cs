93d1f30f10d17e45862095d8e073e871afa9cb30
Keith Geringer <keg7271@gmail.com>
2016-12-04 10:00:03 -0700

    Added data and speechlet classes and files

---
+++ pi-azure-alexa/PiAzureAlexa.Skills.Host/Data/DocumentDbClient.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PiAzureAlexa.Skills.Host.Data
+{
+    public class DocumentDbClient
+    {
+    }
+}
---
+++ pi-azure-alexa/PiAzureAlexa.Skills.Host/Speechlets/PiAzureAlexaSpeechlet.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PiAzureAlexa.Skills.Host.Speechlets
+{
+    public class PiAzureAlexaSpeechlet
+    {
+    }
+}
